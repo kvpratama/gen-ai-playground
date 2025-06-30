dcb57bd361101479fd198331ea36db6ec0a9d326
kvpratama <kvpratama@gmail.com>
2025-06-30 19:08:43 +0900

    Update server.py

--- hf/mcp-course/server.py
+++ hf/mcp-course/server.py
@@ -2,7 +2,7 @@
 from mcp.server.fastmcp import FastMCP
 
 # Create an MCP server
-mcp = FastMCP("Demo")
+mcp = FastMCP("Demo", port=8001)
 
 
 # Add an addition tool
@@ -12,8 +12,18 @@ def add(a: int, b: int) -> int:
     return a + b
 
 
+@mcp.tool()
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers"""
+    return a * b
+
+
 # Add a dynamic greeting resource
 @mcp.resource("greeting://{name}")
 def get_greeting(name: str) -> str:
     """Get a personalized greeting"""
     return f"Hello, {name}!"
+
+# Run the server
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http")
